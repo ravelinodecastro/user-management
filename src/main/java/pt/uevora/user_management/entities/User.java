76e90a4641117ccfbc887cf6d8d78e2e48ff34db
Ravelino Castro <ravelinodecastro@gmail.com>
2026-04-25 08:36:15 +0100

    user entity creation

---
+++ src/main/java/pt/uevora/user_management/entities/User.java
@@ -0,0 +1,21 @@
+package pt.uevora.user_management.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email(message = "O email deve ser válido")
+    private String email;
+
+    private String name;
+}
