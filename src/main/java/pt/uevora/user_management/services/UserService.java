448689eda110bbde8e98858baa71a62c6985996d
Ravelino Castro <ravelinodecastro@gmail.com>
2026-04-25 08:48:15 +0100

    add crud methods in user service

--- src/main/java/pt/uevora/user_management/services/UserService.java
+++ src/main/java/pt/uevora/user_management/services/UserService.java
@@ -2,11 +2,49 @@ package pt.uevora.user_management.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pt.uevora.user_management.dto.UserRequestDTO;
+import pt.uevora.user_management.dto.UserResponseDTO;
+import pt.uevora.user_management.entities.User;
+import pt.uevora.user_management.mappers.UserMapper;
 import pt.uevora.user_management.repositories.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+
+    public UserResponseDTO create(UserRequestDTO dto) {
+        User user = UserMapper.toEntity(dto);
+        return UserMapper.toDTO(repository.save(user));
+    }
+
+    public List<UserResponseDTO> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(UserMapper::toDTO)
+                .toList();
+    }
+
+    public UserResponseDTO getOne(Long id) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Utilizador não encontrado"));
+        return UserMapper.toDTO(user);
+    }
+
+    public UserResponseDTO update(Long id, UserRequestDTO dto) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Utilizador não encontrado"));
+
+        user.setEmail(dto.getEmail());
+        user.setName(dto.getName());
+
+        return UserMapper.toDTO(repository.save(user));
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
